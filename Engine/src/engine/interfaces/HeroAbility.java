759fe8b6db8d5c3f8255aee513a5465e2bddb9c7
trent <trentjmitchell@gmail.com>
2014-08-22 17:39:40 -0700

    This is the engine framework i setup.
    Please change anything you want

---
+++ Engine/src/engine/interfaces/HeroAbility.java
@@ -0,0 +1,11 @@
+package engine.interfaces;
+
+/**
+ * This interface will be the layout hero abilities.
+ * Example: Method to upgrade ability.
+ * @author trent
+ * @version 20140821
+ */
+interface HeroAbility extends MinionAbility {
+
+}
---
+++ Engine/src/engine/interfaces/MinionAbility.java
@@ -0,0 +1,11 @@
+package engine.interfaces;
+
+/**
+ * This interface will be the layout for abilities minions can use.
+ * Example: Implement default method to Cast a spell.
+ * @author trent
+ * @version 20140821
+ */
+interface MinionAbility extends Ability{
+
+}
