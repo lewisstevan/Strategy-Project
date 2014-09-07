93a884dceffab7f5cf33d6855a6e865add2134f6
Stevan Lewis <lewisstevan@gmail.com>
2014-09-07 14:19:43 -0700

    Missing tool added into engine.tools

---
+++ Engine/ENGINEsrc/engine/tools/MapPoint.java
@@ -0,0 +1,14 @@
+package engine.tools;
+
+import java.awt.Point;
+
+public class MapPoint {
+	int x_axis;
+	int y_axis;
+	
+	public MapPoint(int x_axis, int y_axis)
+	{
+		this.x_axis = x_axis;
+		this.y_axis = y_axis;
+	}
+}
