29ec07900ef077e961907a08cc6b3466097f8b21
Andrew Leach <leachad@u.washington.edu>
2014-09-25 14:15:22 -0700

    Initial Commit with a revamped workspace with the most current progress of the game. Made a single comment in order to warrant a commit.

--- Engine/ENGINEsrc/engine/interfaces/AoEAbility.java
+++ Engine/ENGINEsrc/engine/interfaces/AoEAbility.java
@@ -3,6 +3,11 @@ package engine.interfaces;
 public abstract class AoEAbility extends Ability {
 	protected int EffectRadius;
 	
+	/**
+	 * This method will return the area of effected
+	 * radius that the ability "attacks". 
+	 * @return
+	 */
 	protected int getEffectRadius()
 	{
 		return EffectRadius;
